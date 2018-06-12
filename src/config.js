e3a2b9063c244070e33a08b53abead4dc96251fa
Ubuntu <ubuntu@ip-10-112-39-82.eu-central-1.compute.internal>
2018-06-12 16:04:27 +0000

    ui stuff

--- src/config.js
+++ src/config.js
@@ -2,15 +2,15 @@ const _region_ = "eu-central-1";
 
 const dev = {
   s3: {
-    REGION: (self._region_),
+    REGION: (_region_),
     BUCKET: "notes-app2-api-dev-attachmentsbucket-sbi3wz03cuym"
   },
   apiGateway: {
-    REGION: (self._region_),
+    REGION: (_region_),
     URL: "https://8601mxz8ob.execute-api.eu-central-1.amazonaws.com/dev"
   },
   cognito: {
-    REGION: (self._region_),
+    REGION: (_region_),
     USER_POOL_ID: "eu-central-1_Z9sV0HKLI",
     APP_CLIENT_ID: "edjrv40idau5957cg86o33073",
     IDENTITY_POOL_ID: "eu-central-1:a50a1e9e-35fa-4a74-aad2-9b947ddaf46b"
@@ -20,15 +20,15 @@ const dev = {
 
 const prod = {
   s3: {
-    REGION: (self._region_),
+    REGION: (_region_),
     BUCKET: "notes-app2-api-prod-attachmentsbucket-fgqbs9fbqksw"
   },
   apiGateway: {
-    REGION: (self._region_),
+    REGION: (_region_),
     URL: "https://8601mxz8ob.execute-api.eu-central-1.amazonaws.com/prod"
   },
   cognito: {
-    REGION: (self._region_),
+    REGION: (_region_),
     USER_POOL_ID: "eu-central-1_Xazf7paRs",
     APP_CLIENT_ID: "32msgqv0arobobv6vmn7ud55os",
     IDENTITY_POOL_ID: "eu-central-1:e85cf4c6-32a2-4718-81c2-577a274c27d4"
